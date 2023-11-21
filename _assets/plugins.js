3469b8cb83650bd643469a65051fef2bf2a6659a
vvscode <v.vanchuk@tut.by>
2023-11-21 08:51:15 +0000

    Deploying to gh-pages from  @ 180c4f798a9cb679e62148d2442172b419894545 🚀

---
+++ _assets/plugins.js
@@ -0,0 +1,6 @@
+options.dependencies = options.dependencies || [];
+
+options.dependencies.push(
+  // https://github.com/naamor/reveal.js-tableofcontents
+  { src: "_assets/plugins/tableofcontents.js" }
+);
